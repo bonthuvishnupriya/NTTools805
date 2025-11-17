c7278854b18d984a890c2b6e40c0bbc45c62aac5
admin <admin@VenuMadhav>
2025-11-17 13:09:02 +0530

    Dev1 changes2 in StringUtil.java

--- JunitProj01/src/main/java/com/nt/service/StringUtil.java
+++ JunitProj01/src/main/java/com/nt/service/StringUtil.java
@@ -4,25 +4,22 @@ import java.time.LocalDate;
 
 public class StringUtil {
 	public static boolean isPallendrome(String str) {
-		if(str==null || str.equalsIgnoreCase("") || str.length()==0) 
+		if (str == null || str.equalsIgnoreCase("") || str.length() == 0)
 			throw new IllegalArgumentException("Invalid input");
-			String rev=new StringBuffer(str).reverse().toString();
-			return str.equals(rev);
-		}
-	
-	    public LocalDate createDate(int year,int month,int day) {
-	    	if(year<1900 ||month<=0 ||day<=0) {
-	    		return null;
-	    	}
-	    	else {
-	    		return LocalDate.of(year, month, day);
-	    	}
-	    }
-	   public int showLength(String str) {
-		   return str.length();// gives length of string
-	   }
-	    
-	    
+		String rev = new StringBuffer(str).reverse().toString();
+		return str.equals(rev);
 	}
 
+	public LocalDate createDate(int year, int month, int day) {
+		if (year < 1900 || month <= 0 || day <= 0) {
+			return null;
+		} else {
+			return LocalDate.of(year, month, day);
+		}
+	}
 
+	public int showLength(String str) {
+		return str.length();// gives length of the string
+	}
+
+}
